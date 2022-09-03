72d93982f87447afe2478ac8a43d895e5222745c
nuirat <an13cr7@gmail.com>
2022-09-03 16:50:56 +0300

    win

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,17 @@
 import react from "react";
 import { useState } from "react";
 import Board from "./Board";
-
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 export default function Game({ socket, user, room }) {
   const [result, setResult] = useState({ winner: "none", state: "none" });
  const resetGame=()=>{
    setResult({ winner: "none", state: "none" })
   }
+  const { width, height } = useWindowSize()
+
   return (
+    
     <div className="Game">
       <Board
         socket={socket}
@@ -17,7 +21,12 @@ export default function Game({ socket, user, room }) {
         setResult={setResult}
       />
        {result.state === "won" && <div> {result.winner} Won The Game</div>}
-
+    {  result.state === "won"? <Confetti
+      width={width}
+      height={height}
+    />
+:null
+}
       {result.state === "tie" && <div> Game Tieds</div>}
     </div>
   );
