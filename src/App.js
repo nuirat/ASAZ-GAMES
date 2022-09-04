8be2adedfa51bedae9611b82c2aef226faf19208
nuirat <an13cr7@gmail.com>
2022-09-04 22:00:22 +0300

    flappy bird

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Chat from "./components/Chat";
 import Game from "./components/Game";
 
 const App = () => {
   const [user, setUser] = useState("");
   const [room, setRoom] = useState("");
-
   const socket = io.connect("http://localhost:3001");
-
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light'
+  );
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
   const joinRoom = () => {
     if (user && room) socket.emit("joinRoom", room);
   };
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
+    <button onClick={toggleTheme} id="theme">Toggle Theme</button>
       <h3>Join A Chat</h3>
       <input
         type="text"
